9ac78e51649a7f3a7e1830459d494670db55d621
Artyom Kim <artem_kim_2006@list.ru>
2022-12-18 00:46:53 +0600

    created typeormAsyncConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,27 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config/dist'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AppService } from './app.service'
+import { typeOrmAsyncConfig } from './config/typeorm.config'
 import { QuizModule } from './quiz/quiz.module'
 
 @Module({
 	imports: [
+		ConfigModule.forRoot(),
 		QuizModule,
-		TypeOrmModule.forRoot({
-			type: 'mysql',
-			host: 'localhost',
-			port: 3306,
-			username: 'root',
-			password: 'Artyom_2006',
-			database: 'project_1',
-			entities: [__dirname + '/**/*.entity{.ts,.js}'],
-			synchronize: true,
-			logging: true
-		})
+		// TypeOrmModule.forRoot({
+		// 	type: 'mysql',
+		// 	host: 'localhost',
+		// 	port: 3306,
+		// 	username: 'root',
+		// 	password: 'Artyom_2006',
+		// 	database: 'project_1',
+		// 	entities: [__dirname + '/**/*.entity{.ts,.js}'],
+		// 	synchronize: true,
+		// 	logging: true
+		// })
+		TypeOrmModule.forRootAsync(typeOrmAsyncConfig)
 	],
 	controllers: [],
 	providers: [AppService]
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,41 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import {
+	TypeOrmModuleAsyncOptions,
+	TypeOrmModuleOptions
+} from '@nestjs/typeorm'
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-	type: 'mysql',
-	host: 'localhost',
-	port: 3306,
-	username: 'root',
-	password: 'Artyom_2006',
-	database: 'project_1',
-	entities: [__dirname + '/**/*.entity{.ts,.js}'],
-	synchronize: true
+export default class TypeOrmConfig {
+	static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+		return {
+			type: 'mysql',
+			host: configService.get('DB_HOST'),
+			port: configService.get('DB_PORT'),
+			username: configService.get('DB_USERNAME'),
+			password: configService.get('DB_PASSWORD'),
+			database: configService.get('DB_NAME'),
+			entities: [__dirname + '/../**/*.entity.{js,ts}'],
+			synchronize: true,
+			logging: true
+
+			// type: 'mysql',
+			// host: 'localhost',
+			// port: 3306,
+			// username: 'root',
+			// password: 'Artyom_2006',
+			// database: 'project_1',
+			// // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+			// entities: [__dirname + '/../**/*.entity.{js,ts}'],
+			// synchronize: true,
+			// logging: true
+		}
+	}
+}
+
+export const typeOrmAsyncConfig: TypeOrmModuleAsyncOptions = {
+	imports: [ConfigModule],
+	useFactory: async (
+		configService: ConfigService
+	): Promise<TypeOrmModuleOptions> =>
+		TypeOrmConfig.getOrmConfig(configService),
+	inject: [ConfigService]
 }
